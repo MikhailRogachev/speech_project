6b25ae77b2debc9b65acf0c578798d279be7413c
Mikhail Rogachev <Mikhail.rogachev@gmail.com>
2022-02-01 21:43:59 +0100

    added gender evaluation

--- references.py
+++ references.py
@@ -3,3 +3,4 @@ emotions_ref = [ 'Neutral', 'Calm', 'Happy', 'Sad', 'Angry', 'Fearful', 'Disgust
 emotional_intensity_ref = ['normal', 'strong']
 statement_ref = ['Kids are talking by the door', 'Dogs are sitting by the door']
 repetition_ref = ['1st repetition', '2nd repetition']
+gender_ref = ['male', 'female']
