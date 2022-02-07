669fd597fe701cff9ec828ef945530de81134250
Mikhail Rogachev <Mikhail.rogachev@gmail.com>
2022-02-07 20:46:44 +0100

    update structure

--- dataset_loader.py
+++ dataset_loader.py
@@ -2,8 +2,10 @@ import os, time, librosa
 import numpy as np
 from tqdm import tqdm
 from sklearn.model_selection import train_test_split
+from tensorflow.keras import utils
 
 import audio_data_loader as audioloader
+import references as ref
 
 
 def get_features(y, sr):
@@ -65,4 +67,62 @@ def get_dataset(source):
                                                       random_state = 42, 
                                                       shuffle = True)        
         
-    return x_train, x_val, y_train, y_val 
+    return x_train, x_val, y_train, y_val
+
+'''
+This function returns predictors collection
+'''
+def get_x_data(source):
+    
+    x_train = []
+    
+    for i, item in tqdm(source.iterrows()):        
+        y, sr = audioloader.read_audio(item[0])        
+        x_train.append(get_features(y, sr))
+        
+    return np.array(x_train)
+
+'''
+This function returns gender targets collection
+'''
+def get_y_gender_data(source):
+    
+    y_train = []
+    
+    for i, item in tqdm(source.iterrows()):        
+        y_train.append(item[8])
+        
+    return np.array(y_train)
+
+'''
+This function returns emotional intencivity 
+targets collection
+'''
+def get_y_intensivity_data(source):
+    
+    y_train = []
+    
+    for i, item in tqdm(source.iterrows()):        
+        y_train.append(item[4])
+        
+    return np.array(y_train)
+    
+'''
+This function returns emotion targets collection
+'''
+def get_y_emotion_data(source):
+    
+    y_train = []
+    
+    for i, item in tqdm(source.iterrows()):
+        y_train.append(utils.to_categorical(item[3], len(ref.emotions_ref) + 1))
+        
+    return np.array(y_train)
+
+'''
+This function returns data splitted
+'''
+def get_data_split(X, GY, EY, split_size = 0.1):
+    
+    return train_test_split(X, GY, EY, test_size = 0.1, random_state = 42, shuffle = True)    
+    
